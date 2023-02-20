16d042f6f3910ab78ec1c68e6865797afbafc375
Riyaz-Projects <125880336+Riyaz-Projects@users.noreply.github.com>
2023-02-20 17:56:53 +0600

    Add files via upload

---
+++ Systematic/js/main.js
@@ -0,0 +1,8 @@
+// Bx Slider
+$(document).ready(function(){
+   $('.slider_items').bxSlider();
+   jQuery('#main_menu').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: 991,
+   });
+ });
